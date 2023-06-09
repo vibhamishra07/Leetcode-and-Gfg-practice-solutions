bfc5aa0891b209398056101ba1dde0a096949814
Vibha Mishra <88576743+vibhamishra07@users.noreply.github.com>
2023-06-09 18:28:54 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (67.10%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,26 +2,19 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        int freq[nums.size()+1];
-        memset(freq, 0, sizeof(freq));
-        helper(nums, ans, ds, freq);
+        helper(nums, ans, 0);
         return ans;
     }
-    void helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& ds, int* freq){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+    void helper(vector<int>& nums, vector<vector<int>>& ans, int ind){
+        if(ind==nums.size()){
+            ans.push_back(nums);
             return;
         }
         
-        for(int i=0; i<nums.size(); i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                helper(nums, ans, ds, freq);
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i=ind; i<nums.size(); i++){
+            swap(nums[ind], nums[i]);
+            helper(nums, ans, ind+1);
+            swap(nums[ind],  nums[i]);
         }
     }
 };
